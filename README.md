c6a60a785e094f17147146a564b5482c5a47d6ce
Jarrett Cruger <jcrugzz@gmail.com>
2013-08-20 04:26:03 -0400

    [init] FIRST!

---
+++ README.md
@@ -0,0 +1,10 @@
+# back
+
+A simple module to be used for creating exponentially weighted backoff attempts
+stolen from @3rd-Eden, used in [Primus][Primus]
+
+[Primus]: https://github.com/3rd-Eden/primus
+
+## TODO:
+
+Docs and things on how its used
---
+++ index.js
@@ -0,0 +1,38 @@
+
+var Back = module.exports = function reconnect(callback, opts) {
+
+  opts = opts || {};
+
+  this.maxDelay = opts.maxDelay || Infinity;  // Maximum delay.
+  this.minDelay = opts.minDelay || 500;       // Minimum delay.
+  this.retries = opts.retries || 10;          // Amount of allowed retries.
+  this.attempt = (+opts.attempt || 0) + 1;    // Current attempt.
+  this.factor = opts.factor || 2;             // Back off factor.
+
+  // Bailout if we are about to make to much attempts. Please note that we use ...
+  if (opts.attempt > opts.retries || opts.backoff) {
+    return callback(new Error('Unable to retry'), opts);
+  }
+
+  // Prevent duplicate back off attempts.
+  opts.backoff = true;
+
+  //
+  // Calculate the timeout, but make it randomly so we don't retry connections
+  // at the same interval and defeat the purpose. This exponential back off is
+  // based on the work of:
+  //
+  // http://dthain.blogspot.nl/2009/02/exponential-backoff-in-distributed.html
+  //
+  opts.timeout = opts.attempt !== 1
+    ? Math.min(Math.round(
+        (Math.random() * 1) * opts.minDelay * Math.pow(opts.factor, opts.attempt)
+      ), opts.maxDelay)
+    : opts.minDelay;
+
+  setTimeout(function delay() {
+    opts.backoff = false;
+    callback(undefined, opts);
+  }, opts.timeout);
+
+};
